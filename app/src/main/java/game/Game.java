d3783babfcd2e318b643bb82e977b4c6d655f9d6
Matthew Read <matthewreadbass@hotmail.co.uk>
2021-10-27 15:13:09 +0100

    setup project - passing test (questionably)

---
+++ app/src/main/java/game/Game.java
@@ -0,0 +1,16 @@
+package game;
+/**
+ * Game
+ */
+public class Game {
+
+  public static void main(String[] args) {
+    
+  }
+
+  public String getWordToGuess() {
+    return "B_____";
+
+  }
+  
+}
